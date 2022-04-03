a0b41a50af49ae87634963db203f5e98a580d81b
myuantee <55921067+myuantee@users.noreply.github.com>
2022-04-03 13:53:38 +0800

    Update Question1.py

--- Question1.py
+++ Question1.py
@@ -19,8 +19,8 @@ st.write("""
 st.sidebar.header('User input')
 
 
-budget = st.sidebar.slider('Budget',('5000','8000'))
-duration =  st.sidebar.slider('Duration(Days)',('3','4','5','6','7','8'))
+budget = st.sidebar.selectbox('Budget',('3000','4000','5000','6000','7000','8000'))
+duration =  st.sidebar.selectbox('Duration(Days)',('3','4','5','6','7','8'))
 data = {'Budget': budget,
         'Duration': duration}
     
