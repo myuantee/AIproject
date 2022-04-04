247f556f5904f6e8337f9aca9d81da78e18852a4
seejie25 <59280059+seejie25@users.noreply.github.com>
2022-04-04 14:16:56 +0800

    Update question1.py

--- question1.py
+++ question1.py
@@ -15,14 +15,14 @@ budget = st.sidebar.slider("Money On-Hand (RM)", min_value = 1000, max_value = 1
 duration = st.sidebar.slider("Duration (Day)", min_value = 2, max_value = 10, value = 2, step = 1)
 p_count = st.sidebar.slider("Population", min_value = 100, max_value = 1000, value = 100, step = 100)
 n_generation = st.sidebar.slider("Generation", min_value = 100, max_value = 1000, value = 200, step = 100)
-spots = st.sidebar.slider("spots", min_value = 2, max_value = 10, value = 5, step = 1)
-tfre = st.sidebar.slider("transportation frequency",min_value = 1,max_value = 10,value = 5,step = 1)
+spots = st.sidebar.slider("Number of Spots", min_value = 2, max_value = 10, value = 5, step = 1)
+tfre = st.sidebar.slider("Transportation Frequency",min_value = 1,max_value = 10,value = 5,step = 1)
 data = {"Budget" : budget,
         "Duration" : duration,
         "population" : p_count,
         "n_generation" : n_generation,
         "spots": spots,
-        "transportation frequency":tfre}
+        "transportation frequency" : tfre}
 
 budget = int(budget)
 duration = int(duration)
@@ -30,6 +30,20 @@ p_count = int(p_count)
 n_generation = int(n_generation)
 spots = int(spots)
 tfre = int(tfre)
+
+st.write("""---""")
+st.subheader("Method Specifications")
+table = pd.DataFrame()
+table[" "] = ["Selection (%)", "Mutate Position", "From Male", "From Female"]
+table["Method 1"] = ["30", "3", "first 3", "last 3"]
+table["Method 2"] = ["50", "5", "first 1", "last 5"]
+table["Method 3"] = ["10", "1", "first 2", "last 4"]
+
+styler = table.style.hide_index()
+st.write(styler.to_html(), unsafe_allow_html = True)
+
+st.write("""---""")
+st.header("Before Modifying")
 
 # creates a random initial individual (chromosome)
 # specifies the range for each parameter, and incrementation
@@ -49,14 +63,14 @@ def individual():
 def population(count):
     return [individual() for x in range(count)]
 
+def objective(indiv):
+    return (indiv[0]*(duration - 1)) + (indiv[1]*indiv[2]) + (indiv[3]*3*duration) + (indiv[4]*indiv[5]*duration)
+    
 # creates a function to calculate the fitness score for each individual
 # returns the distance between the sum of values and the budget
-def fitness(individual):
-    total = individual[0]*(duration - 1) + individual[1]*individual[2] + individual[3]*3*duration + individual[4]*individual[5]*duration
+def fitness(indiv):
+    total = objective(indiv)
     return abs(budget - total)
-
-def objective(individual):
-    return individual[0]*(duration - 1) + individual[1]*individual[2] + individual[3]*3*duration + individual[4]*individual[5]*duration
 
 # calcultes average fitness of the population
 def grade(pop):
@@ -68,22 +82,21 @@ def evolve(pop, retain, mutate, crossover1, crossover2, random_select = 0.05):
     
     graded = [(fitness(x), x) for x in pop]
     graded = [x[1] for x in sorted(graded)]  # x [1] because x has two component, just take the list --> e.g. [(50,[41,38,86,30,55])]
-    fit = [x[0] for x in sorted(graded)]
     retain_length = int(len(graded)*retain) # how many top % parents to be remainded
     parents = graded[0:retain_length] # get the list of array of individuals as parents - after sorted
 
-    # randomly adds other individuals to promote genetic diversity
-    for individual in graded[retain_length:]: # gets from the remaining individuals NOT selected as parents initially
+    # randomly adds other individuals to promote genetic diversity (selection)
+    for indiv in graded[retain_length:]: # gets from the remaining individuals NOT selected as parents initially
         if random_select > random.random():
-            parents.append(individual)
+            parents.append(indiv)
 
-    # mutates some individuals
-    for individual in parents:
+    # mutates some individuals (mutation)
+    for indiv in parents:
         if mutate > random.random():
-            pos_to_mutate = randint(0, len(individual) - 1)
-            individual[pos_to_mutate] = randint(min(individual), max(individual))
+            pos_to_mutate = randint(0, len(indiv) - 1)
+            indiv[pos_to_mutate] = randint(min(indiv), max(indiv))
 
-    # crossovers parents to create children
+    # crossovers parents to create children (crossover)
     parents_length = len(parents)
     desired_length = len(pop) - parents_length
     children = []
@@ -98,7 +111,6 @@ def evolve(pop, retain, mutate, crossover1, crossover2, random_select = 0.05):
     parents.extend(children)
     
     return parents
-
 
 def ga():
     # creates empty lists to store the results
@@ -123,7 +135,8 @@ def ga():
     
     # calculates sum of the values
     total = solution_list[0]*(duration - 1) + solution_list[1]*solution_list[2] + solution_list[3]*3*duration + solution_list[4]*solution_list[5]*duration
-    st.write("Total vacation expenses: RM", total)
+    total_exp = "Total vacation expenses: RM {}".format(int(total))
+    st.info(total_exp)
 
     parameter = ["Money On-Hand", "Vacation Duration", "Hotel Star Rating", "Tourist Spots", "One Tourist Spot",
                 "Food Price", "Transportation Fees", "Transport Frequency"]
@@ -139,30 +152,18 @@ def ga():
     df.loc[4] = [parameter[4], "< RM{}".format(value[4])]
     df.loc[5] = [parameter[5], "< RM{} per meal".format(value[5])]
     df.loc[6] = [parameter[6], "< RM{} per trip".format(value[6])]
-    df.loc[7] = [parameter[7], "{} trip per day".format(value[7])]
+    df.loc[7] = [parameter[7], "{} trips per day".format(value[7])]
 
     styler = df.style.hide_index()
     st.write(styler.to_html(), unsafe_allow_html = True)
-
+    
     # shows fitness history in dataframe
     hist_df = pd.DataFrame()
     hist_df["Generation"] = [x + 1 for x in list(range(len(fitness_history)))]
     hist_df["Fitness History"] = fitness_history
-
+    
     return fitness_history, final_fit, hist_df
-st.write("# Before Modifying")
-st.write("""---""")
-st.subheader("Method Specifications")
-table = pd.DataFrame()
-table[" "] = ["Selection (%)", "Mutate Position", "From Male", "From Female"]
-table["Method 1"] = ["30", "3", "first 3", "last 3"]
-table["Method 2"] = ["50", "5", "first 1", "last 5"]
-table["Method 3"] = ["10", "1", "first 2", "last 4"]
 
-styler = table.style.hide_index()
-st.write(styler.to_html(), unsafe_allow_html = True)
-
-st.write("#")
 st.subheader("Method 1")
 retain = 0.3
 mutate = 0.01
@@ -186,8 +187,8 @@ crossover1 = 2
 crossover2 = 2
 hist3, fit3, df3 = ga()
 
-st.write("""---""")
-st.subheader("Comparison of Results")
+st.write("#")
+st.subheader("Results Visualization")
 
 st.markdown("Comparing fitness history for each method.")
 merged_df = pd.merge(df1, df2, how = "outer", on = ["Generation"])
@@ -226,12 +227,27 @@ ax.set_ylabel("Fitness")
 ax.set_title("Final Fitness for Each Method")
 st.pyplot(fig)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #after modifying
-st.write("# After Modifying")
+st.write("""---""")
+st.header("After Modifying")
 # creates a random initial individual (chromosome)
 # specifies the range for each parameter, and incrementation
 # returns individual as a list containing 6 numbers (genes)
-
 def individual2(duration, spots, tfre):
     hotel = random.randrange(50, 1000, 5)
     perspots = random.randrange(10, 500, 5)
@@ -292,9 +308,9 @@ def evolve2(pop, retain, mutate, crossover1, crossover2, random_select = 0.05):
     return parents
 
 def ga2():
-# creates empty lists to store the results
+    # creates empty lists to store the results
     value_lst = []
-    fitness_history = []
+    fitness_history = []    
     value_list = []
 
     p = population2(p_count, spots, tfre)
@@ -310,7 +326,7 @@ def ga2():
     # gets the final solution
     final_fit = value_lst[-1]
     solution_list = value_lst[-2]
-    print("Final fitness score:", final_fit)
+    st.write("Final fitness score:", final_fit)
 
     parameter = ["Money On-Hand", "Vacation Duration", "Hotel Star Rating", "Tourist Spots", "One Tourist Spot",
                 "Food Price", "Transportation Fees", "Transport Frequency"]
@@ -324,7 +340,8 @@ def ga2():
 
     # calculates sum of the values
     total = hotel*(duration - 1) +  spots*perspots + food*3*duration + tfee*tfre*duration
-    print("Total vacation expenses: RM", int(total))
+    total_exp = "Total vacation expenses: RM {}".format(int(total))
+    st.info(total_exp)
 
     # shows results in a dataframe
     df = pd.DataFrame(columns = ["Parameter" , "Value"])
@@ -347,13 +364,11 @@ def ga2():
 
     return fitness_history, final_fit, hist_df
 
-st.write("#")
 st.subheader("Method 1")
 retain = 0.3
 mutate = 0.01
 crossover1 = 3
 crossover2 = 3
-
 hist1_2, fit1_2, df1_2 = ga2()
 
 st.write("#")
@@ -372,8 +387,8 @@ crossover1 = 2
 crossover2 = 2
 hist3_2, fit3_2, df3_2 = ga2()
 
-st.write("""---""")
-st.subheader("Comparison of Results")
+st.write("#")
+st.subheader("Results Visualization")
 
 st.markdown("Comparing fitness history for each method.")
 merged_df2 = pd.merge(df1_2, df2_2, how = "outer", on = ["Generation"])
@@ -412,22 +427,27 @@ ax.set_ylabel("Fitness")
 ax.set_title("Final Fitness for Each Method")
 st.pyplot(fig)
 
-st.write("# Comparison")
-fig, axs = plt.subplots(nrows = 2, ncols = 1, figsize=(10,10))
-axs[0].plot(hist1, label = "Method 1")
-axs[0].plot(hist2, label = "Method 2")
-axs[0].plot(hist3, label = "Method 3")
-axs[0].set_xlabel("Generation")
-axs[0].set_ylabel("Fitness")
-axs[0].set_title("Fitness over Generation Before Modifying")
-axs[0].legend()
+st.write("""---""")
+st.subheader("Comparison Before and After Modifying")
 
-axs[1].plot(hist1_2, label = "Method 1")
-axs[1].plot(hist2_2, label = "Method 2")
-axs[1].plot(hist3_2, label = "Method 3")
-axs[1].set_xlabel("Generation")
-axs[1].set_ylabel("Fitness")
-axs[1].set_title("Fitness over Generation After Modifying")
-axs[1].legend()
+st.write("**Before Modifying:**")
+fig, axs = plt.subplots(figsize = (10, 5))
+axs.plot(hist1, label = "Method 1")
+axs.plot(hist2, label = "Method 2")
+axs.plot(hist3, label = "Method 3")
+axs.set_xlabel("Generation")
+axs.set_ylabel("Fitness")
+axs.set_title("Fitness over Generation Before Modifying")
+axs.legend()
+st.pyplot(fig)
 
+st.write("**After Modifying:**")
+fig, axs = plt.subplots(figsize = (10, 5))
+axs.plot(hist1_2, label = "Method 1")
+axs.plot(hist2_2, label = "Method 2")
+axs.plot(hist3_2, label = "Method 3")
+axs.set_xlabel("Generation")
+axs.set_ylabel("Fitness")
+axs.set_title("Fitness over Generation After Modifying")
+axs.legend()
 st.pyplot(fig)
